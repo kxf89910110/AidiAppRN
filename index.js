804d06563543c7e50ed0abcc08224f07126cbf32
kxf89910110 <1052831447@qq.com>
2020-03-05 17:37:19 +0800

    install redux

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
 import {name as appName} from './app.json';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import configureStore from "./src/redux/store";
 
-AppRegistry.registerComponent(appName, () => App);
+const { persistor, store } = configureStore();
+
+const ReduxApp = () => (
+    // 配置 Provider 为根组件，同时传入 store 作为其属性
+    <Provider store={store}>
+        {/* redux 持久化存储 */}
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
